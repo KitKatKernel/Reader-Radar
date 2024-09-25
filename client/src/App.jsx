6b45c8c7d71b082cd9ec2fb5da3c2d88ec6b75bf
KitKatKernel <167586496+KitKatKernel@users.noreply.github.com>
2024-09-25 19:32:55 -0400

    Integrated Apollo Client in App.jsx. Configured Apollo Client with authentication middleware. Set up createHttpLink for GrpahQL.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,40 @@
-import './App.css';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware and attach JWT token to every request
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up client `authLink` middleware prior to making the request
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
